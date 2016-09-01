e7ab7aab2c59169cb8a9d98c7ed855668480aeeb
Surfingnet <maximeghazarian1@gmail.com>
2016-09-01 19:21:32 +0200

    norm ex00

--- d02/ex00/ft_print_alphabet.c
+++ d02/ex00/ft_print_alphabet.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 void ft_print_alphabet(void)
-{
-  for (char c = 'a' ; c <= 'z' ; c++)
+
   {
-    putchar (c);
+    for (char c = 'a' ; c <= 'z' ; c++)
+    {
+      putchar (c);
+    }
+    return ;
   }
-  return;
-}
