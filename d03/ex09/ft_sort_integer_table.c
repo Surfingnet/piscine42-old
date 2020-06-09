87c4be05fcaa553e5c05909fa31414b0c8f912d3
Maxime Ghazarian <mghazari@MacBook-Pro.local>
2020-06-09 09:41:00 +0200

    20200609

--- d03/ex09/ft_sort_integer_table.c
+++ d03/ex09/ft_sort_integer_table.c
@@ -3,38 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   ft_sort_integer_table.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mghazari <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: mghazari <maximeghazarian1@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/09/02 07:03:42 by mghazari          #+#    #+#             */
-/*   Updated: 2016/09/02 07:37:06 by mghazari         ###   ########.fr       */
+/*   Created: 2020/06/09 09:03:05 by mghazari          #+#    #+#             */
+/*   Updated: 2020/06/09 09:36:58 by mghazari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_sort_integer_table(int *tab, int size)
 {
-	int	i;
-	int	again;
-	int tmp;
+	int i, sorted, swap;
 
 	if (size < 2)
-	{
 		return ;
-	}
-	again = 1;
-	while (again == 1)
+	sorted = 0;
+	while (!sorted)
 	{
-		again = 0;
-		i = 1;
-		while (i < size)
-		{
-			if (tab[i] < tab[i - 1])
+		sorted = 1;
+		for (i = 0; i < (size - 1); i++)
+			if (tab[i] > tab[i + 1])
 			{
-				again = 1;
-				tmp = tab[i];
-				tab[i] = tab[i - 1];
-				tab[i - 1] = tmp;
+				swap = tab[i];
+				tab[i] = tab[i + 1];
+				tab[i + 1] = swap;
+				sorted = 0;
 			}
-			i++;
-		}
 	}
+
 }
