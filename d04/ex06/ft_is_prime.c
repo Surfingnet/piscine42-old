2b3f80b497dec69c4e3ecebb6f6ab8e01984b1fc
Maxime Ghazarian <mghazari@MacBook-Pro.local>
2020-10-19 20:18:38 +0200

    20201019

--- d04/ex06/ft_is_prime.c
+++ d04/ex06/ft_is_prime.c
@@ -3,29 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   ft_is_prime.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mghazari <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: mghazari <maximeghazarian1@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/09/03 22:34:10 by mghazari          #+#    #+#             */
-/*   Updated: 2016/09/03 22:59:12 by mghazari         ###   ########.fr       */
+/*   Created: 2020/10/19 18:55:04 by mghazari          #+#    #+#             */
+/*   Updated: 2020/10/19 20:01:45 by mghazari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_is_prime(int nb)
+int ft_is_prime(int nb)
 {
-	int	current_div;
-	int	div_found;
-	int	max_div;
-
-	if (nb < 3)
+	int i, j;
+	if (nb < 2)
 		return (0);
-	current_div = 2;
-	div_found = 0;
-	max_div = nb / 2;
-	while (div_found == 0 && current_div <= max_div)
+	for (i = 2; i <= nb / 2; i++)
 	{
-		if (nb % current_div == 0)
-			div_found = 1;
-		current_div++;
+		if ((i * 2) > nb)
+			return (1);
+		for (j = i; j <= nb / 2; j++)
+		{
+			if ((i * j) == nb)
+				return (0);
+			if ((i * j) > nb)
+				break;
+		}
 	}
-	return (div_found == 0 ? 1 : 0);
+	return (1);
 }
