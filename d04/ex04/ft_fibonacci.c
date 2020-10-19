479d4c7c1019d38605a1a6c299b7a8f1bef60873
Maxime Ghazarian <mghazari@MacBook-Pro.local>
2020-10-19 18:49:46 +0200

    20201019

--- d04/ex04/ft_fibonacci.c
+++ d04/ex04/ft_fibonacci.c
@@ -3,20 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   ft_fibonacci.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mghazari <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: mghazari <maximeghazarian1@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/09/03 21:46:16 by mghazari          #+#    #+#             */
-/*   Updated: 2016/09/03 21:57:17 by mghazari         ###   ########.fr       */
+/*   Created: 2020/10/19 16:25:21 by mghazari          #+#    #+#             */
+/*   Updated: 2020/10/19 18:24:19 by mghazari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_fibonacci(int index)
+int ft_fibonacci(int index)
 {
 	if (index < 0)
 		return (-1);
-	if (index < 1)
-		return (0);
-	if (index < 3)
-		return (1);
-	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+	if (index < 2)
+		return (index);
+	return(ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
 }
