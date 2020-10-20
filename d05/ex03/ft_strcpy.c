f475af3812a85b0be46e0ab9c108dcb05e1e883a
Maxime Ghazarian <mghazari@MacBook-Pro.local>
2020-10-20 14:17:44 +0200

    20201020

--- d05/ex03/ft_strcpy.c
+++ d05/ex03/ft_strcpy.c
@@ -3,43 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mghazari <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: mghazari <maximeghazarian1@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/09/06 18:51:53 by mghazari          #+#    #+#             */
-/*   Updated: 2016/09/06 19:58:35 by mghazari         ###   ########.fr       */
+/*   Created: 2020/10/20 13:56:43 by mghazari          #+#    #+#             */
+/*   Updated: 2020/10/20 14:15:28 by mghazari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
 char	*ft_strcpy(char *dest, char *src)
 {
-	int i;
-	int dest_len;
-	int src_len;
+	int	i, dest_len, src_len;
 
 	dest_len = 0;
-	while (dest[dest_len++] != '\0')
-		continue;
+	while (dest[dest_len++] != '\0');
 	src_len = 0;
-	while (src[src_len++] != '\0')
-		continue;
-	i = -1;
+	while (src[src_len++] != '\0');
 	if (src_len > dest_len)
-		return "";
+		return ("");
+	i = -1;
 	while (src[++i] != '\0')
 		dest[i] = src[i];
 	dest[i] = '\0';
-	return dest;
-}
-
-//TODO
-
-int main(void)
-{
-	char str1[3] = "abc";
-	char str2[3] = "def";
-	ft_strcpy(str2, str1);
-	printf(str2);
-	return 0;
+	return (dest);
 }
